20d342699859cbe37be51574c3f6b9ef253073bd
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2020-06-01 19:43:35 -0500

    create => counter!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+    const [count, setCount] = useState(1);
+
+    const increment = () => setCount(count+1)
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    Learn React
-                </a>
-            </header>
+            <h1>The count is: {count}</h1>
+            <button
+                onClick={increment} 
+                style={{height: '46px', textAlign: 'center'}}>
+                Increment
+            </button>
         </div>
     );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
+
 test('renders learn react link', () => {
-  
+    const wraper = shallow(<App/>)
+    //expect(wraper).toBeTruthy();
 });
