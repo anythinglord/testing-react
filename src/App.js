994975eefdd0686850b039dd86e62c047f5ae65c
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2020-06-03 18:21:50 -0500

    add => data test attributes!

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
 
     const increment = () => setCount(count+1)
     return (
-        <div className="App">
-            <h1>The count is: {count}</h1>
+        <div data-test="component-app">
+            <h1 data-test="counter-display"> The counter is: {count}</h1>
             <button
+                data-test="increment-button"
                 onClick={increment} 
                 style={{height: '46px', textAlign: 'center'}}>
                 Increment
